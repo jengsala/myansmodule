7ce6291f3deb99d683b1da372846f0fd5caf39c4
Adama DIENG <adama.dieng@digitastuces.com>
2024-03-18 13:52:13 +0100

    Personaliser l'affichage de meta

--- library/test.py
+++ library/test.py
@@ -3,6 +3,12 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
+def presentation(module):
+	name = module.params['name']
+	attack = module.params['attack']
+	inventory = module.params['inventory']
+	return {"Presentation" : "My name is {} and my type of attack is {}, here is what you will find in my inventory : {}".format(name, attack, inventory)}
+
 def main():
 
 	fields = {
@@ -17,8 +23,8 @@ def main():
 		"monster": {"default": False, "required": False, "type": "bool"},
 	}
 	module = AnsibleModule(argument_spec=fields)
-	module.exit_json(changed=False, meta=module.params)
-
+	#module.exit_json(changed=False, meta=module.params)
+	module.exit_json(changed=False, meta=presentation(module))
 
 if __name__ == '__main__':
     main()
